3eae19ac6bee487a43d23d0aba4a441a8a939851
Sumit639 <sumitkkumar639@gmail.com>
2023-10-28 22:47:58 +0530

    added the zego cloud

---
+++ app/room/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {ZegoUIKitPrebuilt} from "@zegocloud/zego-uikit-prebuilt"
+const room = () => {
+  return (
+    <>
+    <div>room</div>
+    </>
+  )
+}
+
+export default room
